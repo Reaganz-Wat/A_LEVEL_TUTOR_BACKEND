5df0770ceaa334bc35fee500ac4f08101fc46706
Reaganz-Wat <reaganwatmon6@gmail.com>
2025-04-07 00:19:57 +0300

    feat(auth-controller): add auth-controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+    constructor(
+        private authService: AuthService
+    ) {}
+
+    @Post("signup")
+    signup(@Body() dto: CreateUserDto) {
+
+        return this.authService.signUp(dto);
+    }
+
+    @HttpCode(HttpStatus.OK)
+    @Post("signin")
+    signin(@Body() dto: AuthDto) {
+        return this.authService.signIn(dto);
+    }
+}
